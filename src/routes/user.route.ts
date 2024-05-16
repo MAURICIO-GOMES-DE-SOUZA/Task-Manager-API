2d1af1b7dd8316cef27fde2871a1a32c15682e31
Mauricio Gomes <mauriciodevelopmentweb@gmail.com>
2024-05-16 20:17:17 -0300

    add user create validations

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
 import { userControllers } from "../controllers/userControllers";
 
-export const userRoutes = Router()
+export const userRoutes = Router();
+
+userRoutes.post("/user", userControllers.create);
